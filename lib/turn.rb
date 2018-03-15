faab7cb011605d6228f64f87346842b8795a709a
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-15 17:14:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,9 +40,10 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
+    move(board, 8, "X")
     display_board(board)
   else
     turn(board)
-    move(board, 8, "X")
+    
   end
 end
